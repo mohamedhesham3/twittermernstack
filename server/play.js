a6fb50033fa2af64b1683f1ca5a952c37479c77f
mohamedhesham3 <144559522+mohamedhesham3@users.noreply.github.com>
2024-06-21 15:37:25 +0300

    Update play.js

--- server/play.js
+++ server/play.js
@@ -4,47 +4,39 @@ const Router = require("./Routes/AuthRoute");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const http = require("http");
-const path = require("path");
 const socketIo = require("socket.io");
 const { likes } = require('./Controllers/Post');
 require("dotenv").config();
 
 const { realtimechat } = require("./Controllers/Chat");
-const port = process.env.PORT || 5000; // Use a default port if PORT environment variable is not set
+const port = process.env.PORT 
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'dist')));
 
-// Middleware to handle CORS
+
 const corsOptions = {
-  origin: 'http://localhost:5173/',
+  origin: 'https://twittermernstack.vercel.app',
 };
 app.use(cors(corsOptions));
 
 // Routes
 app.use("/", Router);
 
-// Serve index.html for all other routes to enable client-side routing
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
 
 // WebSocket setup
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "http://localhost:5173/",
+    origin: "https://twittermernstack.vercel.app",
     methods: ["GET", "POST"]
   },
 });
 
-// Initialize socket.io handlers
-likes(io); // Example of how you can use socket.io for likes functionality
-realtimechat(io); // Example of how you can handle real-time chat using socket.io
+likes(io); 
+realtimechat(io);
 
 // Start the server
 server.listen(port, () => {
